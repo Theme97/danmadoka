10b66d1be21463598471fec83c68dd9de1cc26e1
nemui-kun <nickrgooding@gmail.com>
2011-10-27 03:41:47 +0200

    player is now a class, completely redone control scheme, focused shots do more damage, maybe some other stuff idk

--- danmadoka.py
+++ danmadoka.py
@@ -1,10 +1,37 @@
 import pygame, sys
 from pygame.locals import *
 
+class player(pygame.sprite.Sprite):
+	def __init__(self, pos):
+		pygame.sprite.Sprite.__init__(self, self.groups)
+		self.image = pygame.image.load('./homura.png')
+		self.image = self.image.convert()
+		self.rect = self.image.get_rect()
+		self.rect.center = pos
+	def update(self):
+		if keys[keyUp]:
+			self.rect.top -= playerYspeed
+			if self.rect.top < resY/30:
+				self.rect.top = resY/30
+		if keys[keyDown]:
+			self.rect.bottom += playerYspeed
+			if self.rect.bottom > resY-resY/30:
+				self.rect.bottom = resY-resY/30
+		if keys[keyLeft]:
+			self.rect.left -= playerXspeed
+			if self.rect.left < resX/20:
+				self.rect.left = resX/20
+		if keys[keyRight]:
+			self.rect.right += playerXspeed
+			if self.rect.right > resX/20+fieldwidth:
+				self.rect.right = resX/20+fieldwidth
+		global playerPosition
+		playerPosition = self.rect.center
+
 class playerBullet(pygame.sprite.Sprite):
 	def __init__(self, pos):
 		pygame.sprite.Sprite.__init__(self, self.groups)
-		self.image = pygame.image.load('./hom-bulleta.png') # screen res issue
+		self.image = pygame.image.load('./hom-bulleta.png')
 		self.image = self.image.convert()
 		self.rect = self.image.get_rect()
 		self.rect.center = pos
@@ -13,9 +40,7 @@ class playerBullet(pygame.sprite.Sprite):
 	def update(self):
 		self.rect.top -= playerBulletSpeed
 		if self.rect.bottom < resY/30:
-			self.kill()
-		else:
-			pygame.draw.rect(self.image, (0, 0, 0), self.rect)
+			self.kill()		
 	def hit(self):
 		self.kill()
 
@@ -44,130 +69,103 @@ class activeEnemies(pygame.sprite.Sprite):
 	def update(self):
 		if pygame.sprite.spritecollideany(self, playerbullets):
 			playerBullet.hit(pygame.sprite.spritecollideany(self, playerbullets))
-			self.health -= 10
+			self.health -= bulletDamage
 			if self.health <= 0:
 				self.kill()
 
 def level1():
 	levelstart = pygame.time.get_ticks()
-	Enemy((100,100), 20, "./enemy1.png", 2000, levelstart) # screen res issue
-	Enemy((200,100), 50, "./enemy1.png", 5000, levelstart)
 	Enemy((300,250), 30, "./enemy1.png", 0, levelstart)
+	Enemy((250, 100), 60, "./enemy1.png", 1000, levelstart)
+	Enemy((100,100), 20, "./enemy1.png", 2000, levelstart)
+	Enemy((200,150), 50, "./enemy1.png", 5000, levelstart)
 
-# Size config #		
+pygame.init()
+
+# Player config. #
 resX = 640
 resY = 480
-playerwidth = 3*resX/80 # 24 @ 640
-playerheight = resY/12 # 40 @ 480
+keyUp = K_UP
+keyDown = K_DOWN
+keyLeft = K_LEFT
+keyRight = K_RIGHT
+keyShoot = K_z
+keyBomb = K_x # this will do something one day I promise
+keyFocus = K_LSHIFT
+
+# Size #
 fieldwidth = 3*resX/5 # 384 @ 640
 fieldheight = resY-2*resY/30 # 448 @ 480
 
-# Speed config #
+# Speed #
 regXspeed = resX/160 # 4 @ 640
 regYspeed = resY/120 # 4 @ 480
 focusXspeed = resX/320 # 2 @ 640
 focusYspeed = resY/240 # 2 @ 480
 playerBulletSpeed = resY/60 # 8 @ 480
 
-# Misc. config #
+# Misc. #
 msBetweenShots = 150
-playerstartX = resX/20+fieldwidth/2-resX/50
-playerstartY = resY-(fieldheight/10+resY/30+playerheight)
+regbulletDamage = 10
+focusbulletDamage = 20
+playerPosition = (resX/20+fieldwidth/2-resX/50, resY-(fieldheight/10+resY/30+resY/12)) # where player starts
 
 # Stuff #
-goUp = False
-goDown = False
-goLeft = False
-goRight = False
-shooting = False
-lastBullet = 0
-playerXspeed = regXspeed
-playerYspeed = regYspeed
-
-#biggroup = pygame.sprite.LayeredUpdates()
-
-playerbullets = pygame.sprite.Group()
-playerBullet.groups = playerbullets#, biggroup
-
-inactiveEnemies = pygame.sprite.Group()
-Enemy.groups = inactiveEnemies#, biggroup
-
-enemies = pygame.sprite.Group()
-activeEnemies.groups = enemies#, biggroup
-
-pygame.init()
 clock = pygame.time.Clock()
 pygame.display.set_caption('danmadoka')
 window = pygame.display.set_mode((resX, resY), 0, 32)
+keys = pygame.key.get_pressed()
+lastBullet = 0
+
+# Groups #
+playergroup = pygame.sprite.Group()
+player.groups = playergroup
+
+playerbullets = pygame.sprite.Group()
+playerBullet.groups = playerbullets
+
+inactiveEnemies = pygame.sprite.Group()
+Enemy.groups = inactiveEnemies
+
+enemies = pygame.sprite.Group()
+activeEnemies.groups = enemies
 
 field = pygame.Rect(resX/20, resY/30, fieldwidth, fieldheight)
-player = pygame.Rect(playerstartX, playerstartY, playerwidth, playerheight)
+player(playerPosition)
 
 level1()
 
 while True:
-	window.fill((0, 0, 0))
-	pygame.draw.rect(window, (255, 255, 255), field)
-
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			pygame.quit()
 			sys.exit()	
-		if event.type == KEYDOWN:
-			if event.key == K_UP:
-				goUp = True
-			if event.key == K_DOWN:
-				goDown = True
-			if event.key == K_LEFT:
-				goLeft = True
-			if event.key == K_RIGHT:
-				goRight = True
-			if event.key == K_z:
-				shooting = True
-			if event.key == K_LSHIFT:
-				playerXspeed = focusXspeed
-				playerYspeed = focusYspeed
-		if event.type == KEYUP:
-			if event.key == K_UP or event.key == K_w:
-				goUp = False
-			if event.key == K_DOWN or event.key == K_s:
-				goDown = False
-			if event.key == K_LEFT or event.key == K_a:
-				goLeft = False
-			if event.key == K_RIGHT or event.key == K_d:
-				goRight = False
-			if event.key == K_z:
-				shooting = False
-			if event.key == K_LSHIFT:
-				playerXspeed = regXspeed
-				playerYspeed = regYspeed
-	if goUp:
-		player.top -= playerYspeed
-		if player.top < resY/30:
-			player.top = resY/30
-	if goDown:
-		player.bottom += playerYspeed
-		if player.bottom > resY-resY/30:
-			player.bottom = resY-resY/30
-	if goLeft:
-		player.left -= playerXspeed
-		if player.left < resX/20:
-			player.left = resX/20
-	if goRight:
-		player.right += playerXspeed
-		if player.right > resX/20+fieldwidth:
-			player.right = resX/20+fieldwidth
-	if shooting:
+		if event.type == KEYDOWN or event.type == KEYUP:
+			keys = pygame.key.get_pressed()
+
+	bulletDamage = regbulletDamage
+	playerXspeed = regXspeed
+	playerYspeed = regYspeed
+
+	if keys[keyFocus]:
+		bulletDamage = focusbulletDamage
+		playerXspeed = focusXspeed
+		playerYspeed = focusYspeed
+	if keys[keyShoot]:
 		currentTime = pygame.time.get_ticks()
 		if currentTime > lastBullet+msBetweenShots:
-			playerBullet(player.center)
+			playerBullet(playerPosition)
 
+	window.fill((0, 0, 0))
+	pygame.draw.rect(window, (255, 255, 255), field)
+
+	playergroup.update()
 	playerbullets.update()
-	playerbullets.draw(window)
 	inactiveEnemies.update()
 	enemies.update()
+	playerbullets.draw(window)
 	enemies.draw(window)
-	window.blit(pygame.image.load('./homura.png'), player)
+	playergroup.draw(window)
 
 	pygame.display.update()
 	clock.tick(60)
